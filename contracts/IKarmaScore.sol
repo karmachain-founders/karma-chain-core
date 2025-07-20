c3fa34a01f77e9bb78933106a7232898089cf603
buildwithgpt <888sgw@gmail.com>
2025-07-20 13:51:11 -0700

    Update IKarmaScore.sol

--- contracts/IKarmaScore.sol
+++ contracts/IKarmaScore.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./interfaces/IKarmaScore.sol";
+
 interface IKarmaScore {
     function getKarma(address user) external view returns (int256);
     function isAdmin(address admin) external view returns (bool);
 }
+
 
 
