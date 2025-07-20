07e50821b402038b952e61f34de792691cf2a5fd
buildwithgpt <888sgw@gmail.com>
2025-07-20 15:08:57 -0700

    Update iKarmaScore.sol

--- contracts/iKarmaScore.sol
+++ contracts/iKarmaScore.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "contracts/interfaces/iKarmaScore.sol";
-
-interface iKarmaScore {
+interface IKarmaScore {
     function getKarma(address user) external view returns (int256);
     function isAdmin(address admin) external view returns (bool);
+    function updateKarma(address user, int256 change) external;
 }
-
 
 
